8fad2940871339c5b135d0073525086d2c55c1a7
kayhide <kayhide@gmail.com>
2017-02-06 20:55:47 +0900

    create NeuralNetwork.softmax.

--- src/NeuralNetwork.hs
+++ src/NeuralNetwork.hs
@@ -9,3 +9,17 @@ sigmoidFunction x = 1 / (1 + (exp (-x)))
 
 reluFunction :: Double -> Double
 reluFunction = max 0
+
+-- |
+-- >>> softmax [1010, 1000, 990]
+-- [0.999954600070331,4.539786860886666e-5,2.061060046209062e-9]
+-- >>> softmax [0.3, 2.9, 4.0]
+-- [1.821127329554753e-2,0.24519181293507392,0.7365969137693786]
+-- >>> sum $ softmax [0.3, 2.9, 4.0]
+-- 1.0
+softmax :: [Double] -> [Double]
+softmax xs = map (/denom) ys
+  where ys = map calc xs
+        calc x = exp (x - c)
+        c = maximum xs
+        denom = sum ys
