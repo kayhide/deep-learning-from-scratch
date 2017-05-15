3fb8d2e56abb59198348483f49b7976f2fdc8f71
kayhide <kayhide@gmail.com>
2017-05-15 20:40:51 +0900

    add chart outputs.

--- app/Ch4.hs
+++ app/Ch4.hs
@@ -10,7 +10,12 @@ import qualified MNIST
 
 
 main :: IO ()
-main = return ()
+main = do
+  displayFunction1
+  displayTangents
+  displayFunction2
+  displayGradients
+  displayDescents
 
 -- ch4_3_2
 function1 :: Double -> Double
@@ -44,16 +49,17 @@ function2 v = dot v v
 
 displayFunction2 :: IO ()
 displayFunction2 = plotFunc3d attrs [] xs ys function2'
-  where attrs = [(Title "Function 2")]
-        xs = linearScale 100 (-3, 3) :: [Double]
-        ys = linearScale 100 (-3, 3) :: [Double]
-        function2' x y = function2 $ vector [x, y]
+  where
+    attrs = [Title "Function 2"]
+    xs = linearScale 100 (-3, 3) :: [Double]
+    ys = linearScale 100 (-3, 3) :: [Double]
+    function2' x y = function2 $ vector [x, y]
 
 displayGradients :: IO ()
 displayGradients = plotListStyle attrs style vs
   where
-    attrs = [(Title "Gradients of Function 2")]
-    style = defaultStyle { plotType = Vectors }
+    attrs = [Title "Gradients of Function 2"]
+    style = PlotStyle Vectors $ CustomStyle [(LineTitle "gradients")]
     xs = [(-2.0), (-1.75) .. 2.0] :: [Double]
     ys = [(-2.0), (-1.75) .. 2.0] :: [Double]
     vs = do
@@ -65,8 +71,8 @@ displayGradients = plotListStyle attrs style vs
 displayDescents :: IO ()
 displayDescents = plotPathStyle attrs style vs
   where
-    attrs = [(Title "Gradient descents of Function 2"), (XRange (-4, 4)), (YRange (-4, 4))]
-    style = defaultStyle { plotType = Points }
+    attrs = [Title "Gradient descents of Function 2", XRange (-4, 4), YRange (-4, 4)]
+    style = PlotStyle Points $ CustomStyle [(LineTitle "descents")]
     v0 = vector [-3.0, 4.0]
     vs = do
       v <- take 100 $ gradientDescents function2 0.1 v0
